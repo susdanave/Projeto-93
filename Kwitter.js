9b7be94f34fd5e90fa2285b10faf9990c91fdf33
susdanave <121106074+susdanave@users.noreply.github.com>
2023-08-10 08:42:46 -0300

    Add files via upload

---
+++ Kwitter.js
@@ -0,0 +1,12 @@
+function addUser() {
+
+    let nomeUsuario = document.getElementById("nomeUsuario").value;
+  
+    nomeUsuario = document.getElementById("nomeUsuario").value;
+  
+    localStorage.setItem("nomeUsuario", nomeUsuario);
+    
+      window.location = "Kwitter_Room.html";
+  }
+  
+  
